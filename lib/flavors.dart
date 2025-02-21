0cc72b1c6c86d2bbba7ca8c0ba46d57aabd76921
Yahya Maulana Ikhsan <42954636+yamikhsan@users.noreply.github.com>
2025-02-21 18:58:45 +0700

    generated flavors

---
+++ lib/flavors.dart
@@ -0,0 +1,22 @@
+enum Flavor {
+  prod,
+  dev,
+}
+
+class F {
+  static Flavor? appFlavor;
+
+  static String get name => appFlavor?.name ?? '';
+
+  static String get title {
+    switch (appFlavor) {
+      case Flavor.prod:
+        return 'Production App';
+      case Flavor.dev:
+        return 'Development App';
+      default:
+        return 'title';
+    }
+  }
+
+}
